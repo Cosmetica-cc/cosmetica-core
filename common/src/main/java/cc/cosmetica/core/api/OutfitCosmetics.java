eb0c3a5fef2cee58cfa1d284b2ee05e77142a4a5
valoeghese <46117728+valoeghese@users.noreply.github.com>
2025-11-03 13:11:29 +1300

    chore: debugging

--- common/src/main/java/cc/cosmetica/core/api/OutfitCosmetics.java
+++ common/src/main/java/cc/cosmetica/core/api/OutfitCosmetics.java
@@ -41,7 +41,7 @@ public class OutfitCosmetics implements Cosmetics {
 		this.creator = Cosmetic.gameProfileOf(outfit.getCreator());
 		this.accessories = new ArrayList<>();
 
-
+//		System.out.println(outfit);
 
 		// read accessories
 		for (OutfitAccessory accessory : outfit.getAccessories()) {
