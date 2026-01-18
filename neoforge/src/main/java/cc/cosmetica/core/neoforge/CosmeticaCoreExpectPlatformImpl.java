b079f71a879ab7f60cd3a0e0b52c5232e41b84b5
valoeghese <46117728+valoeghese@users.noreply.github.com>
2026-01-18 20:53:31 +1300

    fix: Add expect platform to neoforge

---
+++ neoforge/src/main/java/cc/cosmetica/core/neoforge/CosmeticaCoreExpectPlatformImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024, 2025 Cosmetica
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cc.cosmetica.core.neoforge;
+
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+
+/**
+ * Forge implementation of CosmeticaCore expect platform.
+ */
+public class CosmeticaCoreExpectPlatformImpl {
+	public static Path getGameDirectory() {
+		return FMLPaths.GAMEDIR.get();
+	}
+
+	public static Path getConfigDirectory() {
+		return FMLPaths.CONFIGDIR.get();
+	}
+
+	public static boolean isDev() {
+		return !FMLEnvironment.production;
+	}
+}
